8b58d017b0bcbcd36f23c64edd0ec317cf345ee0
GevorkyanLiya45 <lg.lap.0405@gmail.com>
2026-06-09 15:59:06 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+﻿import unittest
+from main import validate_password, filter_positive_numbers
+
+class TestLogicChecker(unittest.TestCase):
+
+    def test_password_validation(self):
+        assert validate_password("P1a") == False, "Ошибка: пароль слишком короткий"
+        assert validate_password("Pass1234") == True, "Ошибка: правильный пароль не прошел"
+
+    def test_filter_numbers(self):
+        result = filter_positive_numbers("1, -5, 0")
+        assert 0 not in result, "Ошибка: 0 не должен быть в списке"
+        assert result == [1.0], "Ошибка: список отфильтрован неверно"
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testnew.py
@@ -0,0 +1,20 @@
+﻿import unittest
+from mainnew import validate_password, filter_positive_numbers
+
+class TestLogicChecker(unittest.TestCase):
+
+    def test_password_validation(self):
+        assert validate_password("P1a") == False, "Ошибка: пароль слишком короткий"
+        assert validate_password("Pass1234") == True, "Ошибка: правильный пароль не прошел"
+
+    def test_filter_numbers(self):
+        result = filter_positive_numbers("1, -5, 0, 10")
+        assert 0 not in result, "Ошибка: 0 не должен быть в списке"
+        assert result == [1.0, 10.0], "Ошибка: список отфильтрован неверно"
+
+    def test_filter_type_error(self):
+        with self.assertRaises(TypeError):
+            filter_positive_numbers(123)
+
+if __name__ == '__main__':
+    unittest.main()
